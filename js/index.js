69f04015adeebb139e5b2e853d1b2718577d87b3
Mcampos023 <54911444+Mcampos023@users.noreply.github.com>
2023-01-25 00:22:01 -0600

    misael casa
    ajuste por cambio a funcion

--- js/index.js
+++ js/index.js
@@ -5,11 +5,13 @@ function quitaacentos(){
 }
 
 
-const sincaracteresespeciales = (str) => {
-	return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '') // para acentos
-		//.replace(/([^\w]+|\s+)/g, '-') // sin espacios este lo quito mejor
-		.replace(/\-\-+/g, '-')	// sin saltos
-		.replace(/(^-+|-+$)/g, ''); // sin cosas otros caracteres al final
+function sincaracteresespeciales(str) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '') // para acentos
+
+        //.replace(/([^\w]+|\s+)/g, '-') // sin espacios este lo quito mejor
+        .replace(/\-\-+/g, '-') // sin saltos
+        .replace(/(^-+|-+$)/g, ''); // sin cosas otros caracteres al final
 }
+
 
 
